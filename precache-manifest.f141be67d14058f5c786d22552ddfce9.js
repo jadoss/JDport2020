88d3b3e440ecd76d0481620f2ffecd05822a98fa
x <x@xs-MacBook-Pro.local>
2020-03-09 15:05:38 -0500

    Updates

---
+++ precache-manifest.f141be67d14058f5c786d22552ddfce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3850418f0f11a2afcd30623c8cbd9f06",
+    "url": "/JDport2020/index.html"
+  },
+  {
+    "revision": "ae56850d2b049d5cfb37",
+    "url": "/JDport2020/static/js/2.e9b8f308.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/JDport2020/static/js/2.e9b8f308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8277a67ebc4465de4974",
+    "url": "/JDport2020/static/js/main.a5dcbafa.chunk.js"
+  },
+  {
+    "revision": "d964ea56de854b16eee5",
+    "url": "/JDport2020/static/js/runtime-main.d6ea69b5.js"
+  },
+  {
+    "revision": "ffce2b0d6276f4336e476680bc6bc382",
+    "url": "/JDport2020/static/media/profile.ffce2b0d.png"
+  }
+]);
